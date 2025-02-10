76d00ef92346e34f81b11a3e3d9c2967c4c6e72d
Sampurno Sarkar <sampurnosarkar.ind@gmail.com>
2025-02-10 21:33:20 +0530

    vercel.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { HashRouter, Route, Routes } from "react-router-dom";
+import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './Home';
 import Events from './Events';
 import Members from './Members';
@@ -8,16 +8,20 @@ import Fixtures from './Fixtures';
 
 const App = () => {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/members" element={<Members />} />
-        <Route path="/registrations" element={<Registrations />} />
-        <Route path="/fixtures" element={<Fixtures />} />
-      </Routes>
-    </HashRouter>
-  );
-};
+    <div>
+        <BrowserRouter>
+        <Routes>
+         
+          <Route path="/" element={<Home />} />
+          <Route path="/events" element={<Events/>} />
+          <Route path="/members" element={<Members/>} />
+          <Route path="/registrations" element={<Registrations/>} />
+          <Route path="/fixtures" element={<Fixtures/>} />
+          
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
 
-export default App;
+export default App
